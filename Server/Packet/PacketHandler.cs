d49854e7c67a2677869d7c42109f94f4755cfad8
Hyun Soo An <ahs20103@gmail.com>
2024-07-04 09:55:13 +0900

    Initial commit

---
+++ Server/Packet/PacketHandler.cs
@@ -0,0 +1,40 @@
+﻿using Server;
+using Server.Session;
+using ServerCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+class PacketHandler
+{
+	public static void C_LeaveGameHandler(PacketSession session, IPacket packet)
+	{        
+		ClientSession clientSession = session as ClientSession;
+
+        if (clientSession.Room == null)
+        {
+            return;
+        }
+
+        GameRoom room = clientSession.Room;
+        room.Push( () => room.Leave(clientSession) );
+
+        //clientSession.Room.Broadcast(clientSession, chatPacket.chat);
+    }
+
+    public static void C_MoveHandler(PacketSession session, IPacket packet)
+    {
+        C_Move movePacket = packet as C_Move;
+        ClientSession clientSession = session as ClientSession;
+
+        if (clientSession.Room == null)
+        {
+            return;
+        }
+
+        GameRoom room = clientSession.Room;
+        room.Push(() => room.Move(clientSession, movePacket));
+
+        //clientSession.Room.Broadcast(clientSession, chatPacket.chat);
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using static System.Collections.Specialized.BitVector32;
+using ServerCore;
+using Server.Session;
+
+namespace Server
+{
+    internal class Program
+    {
+        static Listener _listener = new Listener();
+        public static GameRoom Room = new GameRoom();
+         
+        static void Main(string[] args)
+        {
+            // DNS (Domain Name System)
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+
+            _listener.Init(endPoint, () => { return SessionManager.instance.Generate(); });
+            Console.WriteLine("Listening....");
+
+            while (true)
+            {
+                Room.Push(() => Room.Flush());
+                Thread.Sleep(250);
+            }
+
+
+        }
+    }
+}
